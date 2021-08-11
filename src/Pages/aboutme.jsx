cce8935391ebb06ce7e3b11cb9af482b74eac1f2
seacrest <cassiep1986@gmail.com>
2021-08-11 16:07:08 -0700

    Updated abput me style

--- src/Pages/aboutme.jsx
+++ src/Pages/aboutme.jsx
@@ -10,14 +10,13 @@ function Aboutme() {
 
       <div >
         <section className="text">
-          <p>
-            Hello, my name is Cassandra Pacheco and I am a (soon to be)
-            certified full-stack web developer. Although new to web development,
-            I (will soon) have completed a full-stack program with the
+          <p className = "aboutme">
+            Hello, my name is Cassandra Pacheco and I am a full-stack web developer. Although new to web development,
+            I have completed a full-stack coding bootcamp program with the
             University of Arizona learning the following languages: HTML, CSS,
             Javascript (including Node.JS), SQL. I also have experience with the
-            following frameworks/libraries: React, Sequelize, Express, Inquirer,
-            Bootstrap, Bulma, jQuery,{" "}
+            following frameworks/libraries: React, Sequelize, Express, Inquirer, Mongodb,
+            Bootstrap, Bulma, jQuery, and Material UI. 
           </p>
         </section>
       </div>
