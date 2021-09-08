18aed2a78db67c46fd6b4fce3349f210b4174b5a
seacrest <cassiep1986@gmail.com>
2021-09-07 23:45:00 -0700

    adjusted text on aboutme

--- src/Pages/aboutme.jsx
+++ src/Pages/aboutme.jsx
@@ -10,7 +10,7 @@ function Aboutme() {
       <div >
         <section className="text">
           <p className = "aboutme">
-          I am a recent graduate of the Full Stack Web Development course at the University of Arizona with a background in pharmacy. With newly developed skills in HTML, CSS, JavaScript, React.js, MySQL,Graph QL, and other frameworks/libraries, I have created  applications for both practice and demonstration purposes. 
+          I am a recent graduate of the Full Stack Web Development course at the University of Arizona with a background in pharmacy. With newly developed skills in HTML, CSS, JavaScript, React.js, MySQL,Graph QL, and other frameworks/libraries, I have created applications for both practice and demonstration purposes. 
           
           Known for my passion of organization and efficiency, my objective is to create an experience that is both user friendly and engaging. As an experienced leader and collaborator, I am thrilled to use both my well developed and newly learned skills to become an integral team member in the creation and improvement of client/user experiences online.   
           </p>
