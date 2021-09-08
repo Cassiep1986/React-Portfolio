f5baf9f1c9791f790fed7a9a92bbdb99f9560273
seacrest <cassiep1986@gmail.com>
2021-09-07 22:28:53 -0700

    made navbar responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,10 +3,10 @@ import {NavLink} from 'react-router-dom'
 function Header() {
   return (
     <header>
-      <h1 id="name">Cassandra Pacheco</h1>
 
-      <nav className="topnav">
-
+      <nav className="navbar navbar-expand-md topnav">
+        <div className="container-fluid">
+        <a class="navbar-brand" href="#"><img src="./images/name_img.PNG" alt="Cassandra Pacheco" width="" height=""/></a>
         <NavLink to="/" exact activeStyle={{color:"violet"}}>About Me</NavLink>
 
         <NavLink to="/portfolio"activeStyle={{color:"violet"}}>Portfolio</NavLink>
@@ -14,6 +14,7 @@ function Header() {
         <NavLink to="/contact"activeStyle={{color:"violet"}}>Contact Me</NavLink>
 
         <NavLink to="/resume"activeStyle={{color:"violet"}}>Resume</NavLink>
+      </div>
       </nav>
 
     </header>
